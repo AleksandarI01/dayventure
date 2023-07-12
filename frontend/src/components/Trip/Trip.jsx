257dc30ce7adcc364360ce5e29465b8760aa35be
Aleksandar Ilic <aleks.i6@hotmail.com>
2023-07-12 14:27:47 +0200

    Trip component created

---
+++ frontend/src/components/Trip/Trip.jsx
@@ -0,0 +1,13 @@
+const Trip = () => {
+    return (
+        <>
+            <div
+                className="w-[272px] h-[400px] shrink-0 flex flex-col gap-[0.2rem] border border-solid rounded-md border-venture-green">
+
+            </div>
+
+        </>
+    );
+};
+
+export default Trip;
--- frontend/src/pages/UserProfile/index.jsx
+++ frontend/src/pages/UserProfile/index.jsx
@@ -1,7 +1,9 @@
+import Trip from "../../components/Trip/Trip.jsx"
 const UserProfile = () => {
   return (
     <>
       <h1>UserProfile</h1>
+        <Trip></Trip>
     </>
   );
 };
