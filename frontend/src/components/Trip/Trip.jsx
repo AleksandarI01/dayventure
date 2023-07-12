50c1fa0f1f4be898fac0bb34475354f3deaf5832
Aleksandar Ilic <aleks.i6@hotmail.com>
2023-07-12 17:30:22 +0200

    created ImageTrip and Trip component (not finished)

--- frontend/src/components/Trip/Trip.jsx
+++ frontend/src/components/Trip/Trip.jsx
@@ -1,8 +1,52 @@
+import Label from "../Label/Label.jsx"
+import { RiStarSFill } from "react-icons/ri";
+
 const Trip = () => {
     return (
         <>
             <div
-                className="w-[272px] h-[400px] shrink-0 flex flex-col gap-[0.2rem] border border-solid rounded-md border-venture-green">
+                className="w-[272px] h-[400px] shrink-0 flex flex-col gap-[0.2rem] border border-solid rounded-md border-venture-gray">
+                <div className="h-[70%] w-[100%] border border-solid border-[red]">
+
+                </div>
+                <div className="h-[30%] w-[100%]">
+                    <div className="h-[40%] w-[100%] shrink-0 flex flex-row gap-[0.2rem]">
+                        <div className="h-[100%] w-[50%] flex flex-col ">
+                            <div className="h-[50%] w-[100%]">
+                                <p className="text-left pl-1 font-medium">My cool Trip</p>
+
+                            </div>
+                            <div className="h-[50%] w-[100%]">
+                                <p className="text-left pl-1 font-medium">Location: Zürich</p>
+                            </div>
+
+
+                        </div>
+                        <div className="h-[100%] w-[50%] border border-solid border-[orange]">
+
+
+
+                        </div>
+
+
+                    </div>
+                    <div className="h-[60%] w-[100%] flex flex-col shrink-0">
+                        <div className="h-[35%] w-[100%]">
+                            <p className="text-left pl-1">created by: Aleks 4 lv</p>
+                        </div>
+                        <div className="h-[65%] w-[100%] pl-1 flex flex-row shrink-0 gap-[0.2rem] flex-wrap">
+                            <Label></Label>
+                            <Label></Label>
+                            <Label></Label>
+                            <Label></Label>
+                        </div>
+
+
+                    </div>
+
+
+                </div>
+
 
             </div>
 
