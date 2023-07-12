e2c2f86edd964fc042033f72f816dd20cdf82f3d
iamlydial <lydia.lavecchia@gmail.com>
2023-07-12 15:40:34 +0200

    header and footer components added and styled

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,45 @@
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
+const Footermenu = [
+  { name: "About us", link: "/aboutus" },
+  { name: "Press", link: "/press" },
+  { name: "Blog", link: "/blog" },
+  { name: "Ios", link: "/ios" },
+  { name: "Andriod", link: "/andriod" },
+];
+
+const SocialMedia = [
+  { name: "Facebook", icon: <FaFacebook />, link: "www.facebook.com" },
+  { name: "Twitter", icon: <FaTwitter />, link: "www.twitter.com" },
+  { name: "TikTok", icon: <FaTiktok />, link: "www.tiktok.com" },
+  { name: "Instagram", icon: <FaInstagram />, link: "www.instagram.com" },
+];
+
+console.log(FaFacebook);
 const Footer = () => {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <footer className="flex justify-between fixed w-full p-5 left-0 bottom-0">
+      <ul className="flex gap-[4.7rem]">
+        {Footermenu.map((item, idx) => (
+          <li key={idx}>
+            <NavLink to={item.link}>{item.name}</NavLink>
+          </li>
+        ))}
+      </ul>
+      <ul className="flex gap-[1.4rem]">
+        {SocialMedia.map((item) => (
+          <li>
+            <a href={item.link} target="_blank">
+              {item.icon}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </footer>
   );
 };
 
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,16 +1,71 @@
-import Button from "../Button/Button";
-import Label from "../Label/Label";
+import { NavLink, useNavigate, Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../store/slices/user.js";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const access_token = useSelector((state) => state.user.accessToken);
+  const [MenuItems, SetMenuItems] = useState([]);
+
+  useEffect(() => {
+    SetMenuItems([
+      { name: "Home", link: "/" },
+      { name: "Search", link: "/search" },
+      { name: "Profile", link: access_token ? "/profile" : "/login" },
+    ]);
+  }, [access_token]);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(logout());
+    localStorage.removeItem("refreshToken");
+    navigate("/");
+  };
+
   return (
-    <>
-      <h1>Header 1 example</h1>
-      <h2>Header 2 example</h2>
-      <h3>Header 3 example</h3>
-      <p>Test paragraph</p>
-      <Button />
-      <Label />
-    </>
+    <header className="flex justify-between border-b-venture-black relative p-5 border-b border-solid">
+      <Link to="/" className="flex gap-4">
+        <h4>dayventure</h4>
+      </Link>
+      <nav className="flex gap-[50px] items-center">
+        <ul className="flex gap-[50px] items-center">
+          {MenuItems.map((item, idx) => (
+            <li key={idx}>
+              <NavLink to={item.link}>{item.name}</NavLink>
+            </li>
+          ))}
+        </ul>
+        {access_token ? (
+          <button
+            className="text-base text-venture-black uppercase bg-venture-green cursor-pointer px-5 py-2 border-[none]"
+            onClick={handleLogout}
+          >
+            logout
+          </button>
+        ) : (
+          <div className="flex gap-0.5">
+            <button
+              className="text-base text-venture-black uppercase bg-venture-green cursor-pointer px-5 py-2 border-[none] rounded-[18px_0_0_18px]"
+              onClick={() => {
+                navigate("/signup");
+              }}
+            >
+              signup
+            </button>
+            <button
+              className="text-base text-venture-black uppercase bg-venture-green cursor-pointer px-5 py-2 border-[none] rounded-[0px_18px_18px_0px]"
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              login
+            </button>
+          </div>
+        )}
+      </nav>
+    </header>
   );
 };
 
