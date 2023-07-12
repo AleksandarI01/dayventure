0d86292d79e1ed75d5527d336b38bba18358aae3
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-07-12 23:54:33 +0200

    Add navigation to login page

--- frontend/src/components/SignUp/SignUpThirdStep.jsx
+++ frontend/src/components/SignUp/SignUpThirdStep.jsx
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Button from "../Button/Button";
 import AnimatedSignUpImage from "../AnimatedSignUpImage/AnimatedSignUpImage";
+
 const SignUpThirdStep = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // add your signup logic here
+    navigate("/login");
+  };
 
     return (
       <>
-        <form> 
-          <input type="text" placeholder="Validation Code" />
-          <input type="email" placeholder="Email" />
-          <input type="text" placeholder="Username" />
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Confirma Password" />
-          <AnimatedSignUpImage />
-          <Button>SUBMIT</Button>
+        <form className="flex flex-col items-center  gap-10" onSubmit={handleSubmit}> 
+          <input type="text" placeholder="Validation Code" className="w-full"/>
+          <div className="flex gap-5">
+            <input type="email" placeholder="Email" />
+            <input type="text" placeholder="Username" />
+          </div>
+          <div className="flex gap-5">
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+          </div>
+          <div className="flex gap-5">
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm Password" />
+          </div>
+          <AnimatedSignUpImage/>
+          <Button type="submit">SUBMIT</Button>
         </form>
       </>
     );
