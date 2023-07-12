d8b32b959dba5ee281ecfcb383b3ddc1ad85c34d
iamlydial <lydia.lavecchia@gmail.com>
2023-07-12 09:56:16 +0200

    branches and pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./layout";
+import Home from "./pages/Home";
+import Signup from "./pages/SignUp";
+import Login from "./pages/Login";
+import Search from "./pages/Search";
+import UserProfile from "./pages/UserProfile";
+import AboutUs from "./pages/AboutUs";
+import NewTrip from "./pages/NewTrip";
+import Trip from "./pages/Trip";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/search/users/:userId" element={<UserProfile />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/new-trip" element={<NewTrip />} />
+          <Route path="/trip" element={<Trip />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
