6079c598dbb3e26f048e77c1e0416abfbd1bdc5a
iamlydial <lydia.lavecchia@gmail.com>
2023-07-12 14:10:24 +0200

    created global styling, button and label component

---
+++ frontend/src/components/Label/Label.jsx
@@ -0,0 +1,11 @@
+const Label = ({ type, onClickFunction, children }) => {
+  return (
+    <>
+      <div className={"bg-venture-darkgray rounded-full w-fit px-3 py-0.5"}>
+        <p className={"text-xs text-venture-white"}>hopspitality</p>
+      </div>
+    </>
+  );
+};
+
+export default Label;
