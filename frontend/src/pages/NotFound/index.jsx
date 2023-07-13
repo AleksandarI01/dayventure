0995e7ecb4c3f79dd0cfd34e37430f63b95b6190
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-07-13 16:40:11 +0200

    Add redirection function and update spacing

--- frontend/src/pages/NotFound/index.jsx
+++ frontend/src/pages/NotFound/index.jsx
@@ -1,25 +1,47 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Button from "../../components/Button/Button";
 import NotFoundImage from "../../assets/images/404.png";
+
 const NotFound = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  // Save the previous location when the current location changes
+  useEffect(() => {
+    localStorage.setItem("prevLocation", location.pathname);
+  }, [location]);
 
   const handleOnClick = (e) => {
     e.preventDefault();
-    // add your signup logic here
-    navigate("/");
+    // If there's a previous location, navigate to it
+    let prevLocation = localStorage.getItem("prevLocation");
+    console.log("PrevLocation: ", prevLocation);
+    if (prevLocation) {
+      navigate(prevLocation);
+    } else {
+      navigate("/");
+    }
   };
+
   return (
     <>
-      <div className="h-[87.5vh] flex flex-col justify-center items-center gap-5">
+      <div className="h-[87.5vh] flex flex-col justify-center items-center gap-12">
         <h1>Error 404</h1>
-        <p>You must have taken a wrong turn.<br />Let us bring you back on track!</p>
-        <img src={NotFoundImage} alt="NotFoundImage" className="h-[16vh] w-[16vh]"></img>
-        <Button onClickFunction={(e) => handleOnClick(e)}>Home</Button>
+        <p>
+          You must have taken a wrong turn.
+          <br />
+          Let us bring you back on track!
+        </p>
+        <img
+          src={NotFoundImage}
+          alt="NotFoundImage"
+          className="h-[16vh] w-[16vh]"
+        ></img>
+        <Button onClickFunction={handleOnClick}>Go Back</Button>
       </div>
     </>
   );
 };
 
 export default NotFound;
-
