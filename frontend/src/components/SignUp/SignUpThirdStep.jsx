3a44e61279fa8edd1c181101916161715bfeceb9
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-07-13 12:55:50 +0200

    Add icons to inputfields

--- frontend/src/components/SignUp/SignUpThirdStep.jsx
+++ frontend/src/components/SignUp/SignUpThirdStep.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import InputField from "../InputField/InputField";
 import Button from "../Button/Button";
 import AnimatedSignUpImage from "../AnimatedSignUpImage/AnimatedSignUpImage";
 
@@ -16,18 +16,18 @@ const SignUpThirdStep = () => {
     return (
       <>
         <form className="flex flex-col items-center  gap-10" onSubmit={handleSubmit}> 
-          <input type="text" placeholder="Validation Code" className="w-full"/>
+          <InputField type="text" placeholder="Validation Code"/>
           <div className="flex gap-5">
-            <input type="email" placeholder="Email" />
-            <input type="text" placeholder="Username" />
+            <InputField type="email" placeholder="Email" />
+            <InputField type="text" placeholder="Username" />
           </div>
           <div className="flex gap-5">
-            <input type="text" placeholder="First Name" />
-            <input type="text" placeholder="Last Name" />
+            <InputField type="text" placeholder="First Name" />
+            <InputField type="text" placeholder="Last Name" />
           </div>
           <div className="flex gap-5">
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Confirm Password" />
+            <InputField type="password" placeholder="Password" />
+            <InputField type="password" placeholder="Confirm Password" />
           </div>
           <AnimatedSignUpImage/>
           <Button type="submit">SUBMIT</Button>
