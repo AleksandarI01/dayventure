da222133e2489674bfe66bb06cedddf751280103
Aleksandar Ilic <aleks.i6@hotmail.com>
2023-07-13 14:40:10 +0200

    started Search&Filter page

--- frontend/src/pages/Search/index.jsx
+++ frontend/src/pages/Search/index.jsx
@@ -1,9 +1,26 @@
+import SearchHeader from "../../components/SearchHeader/SearchHeader.jsx";
+import Label from "../../components/Label/Label.jsx";
+
 const Search = () => {
-  return (
-    <>
-      <h1>Search</h1>
-    </>
-  );
+    return (
+        <>
+            <div
+                className="flex flex-col align-middle justify-center w-full items-center bg-KayakHomeHeader h-96 bg-no-repeat bg-cover">
+                <div className={"w-full h-[23%]"}>
+                </div>
+
+                <SearchHeader></SearchHeader>
+                <div className="flex flex-row py-3">
+                    <Label>Hospitality</Label>
+                    <Label>Museum</Label>
+                    <Label>Shopping</Label>
+                    <Label>Food</Label>
+                    <Label>Sport</Label>
+                    <Label>Hiking</Label>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Search;
