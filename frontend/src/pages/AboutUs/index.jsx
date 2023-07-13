bc987785c4a079837cb5f194d37f4b6088d7ff80
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-07-13 16:41:46 +0200

    Add structure and placeholders to aboutUs page

--- frontend/src/pages/AboutUs/index.jsx
+++ frontend/src/pages/AboutUs/index.jsx
@@ -1,8 +1,23 @@
+import ImageTrip from "../../components/ImageTrip/ImageTrip.jsx";
 const AboutUs = () => {
   return (
-    <>
-      <h1>AboutUs</h1>
-    </>
+    <div className="flex flex-col items-center justify-center w-full bg-KayakHomeHeader h-96 bg-no-repeat bg-cover">
+      <div className="flex-col text-venture-white my-6">
+        <h1>ORGANIZE YOUR PERFECT DAY-TRIP</h1>
+        <h3>BY USERS, FOR USERS</h3>
+      </div>
+      <div className="flex flex-row w-full justify-center p-8">
+        <div className="flex flex-col w-1/3 items-center">
+          <ImageTrip className="text-4xl my-3" />
+        </div>
+        <div className="flex flex-col w-1/3 items-center">
+          <ImageTrip className="text-4xl my-3" />
+        </div>
+        <div className="flex flex-col w-1/3 items-center">
+          <ImageTrip className="text-4xl my-3" />
+        </div>
+      </div>
+    </div>
   );
 };
 
