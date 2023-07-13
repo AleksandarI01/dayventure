551064851895d0fe25c75617aed2e1863604727c
Stefan Lustenberger <chrott77@gmail.com>
2023-07-13 11:30:11 +0200

    added icon to category

--- backend/category/models.py
+++ backend/category/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 
+def category_image_directory_path(instance, filename):
+    return f'category/{filename}'
+
+
 class Category(models.Model):
     name = models.CharField(max_length=150, unique=True)
+    icon = models.ImageField(upload_to=category_image_directory_path, blank=True, null=True)
 
     def __str__(self):
         return f'trip : {self.name}'
-
-
