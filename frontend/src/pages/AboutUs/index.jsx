b195123fe7a6ee263a16862813c170535fbc53f8
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-07-14 16:52:43 +0200

    Add responsiveness for md size screens

--- frontend/src/pages/AboutUs/index.jsx
+++ frontend/src/pages/AboutUs/index.jsx
@@ -3,7 +3,7 @@ import Trip from "../../components/Trip/Trip";
 import ContactUsCard from "../../components/ContactUsCard/ContactUsCard";
 const AboutUs = () => {
   return (
-    <div className="h-[87.5vh] flex flex-col gap-20">
+    <div className="h-[87.5vh] flex flex-col gap-20 flex-grow overflow-auto">
       <div className="flex flex-col items-center justify-center w-full bg-BgAboutUs h-96 bg-no-repeat bg-cover">
         <div className="flex-col text-venture-white my-6">
           <h1>ORGANIZE YOUR PERFECT DAY-TRIP</h1>
@@ -11,7 +11,7 @@ const AboutUs = () => {
         </div>
       </div>
 
-      <div className=" flex flex-row w-full justify-evenly p-8">
+      <div className=" flex flex-col md:flex-row items-center w-full justify-center p-8 gap-20">
         <TeamCard />
         <Trip />
         <ContactUsCard />
