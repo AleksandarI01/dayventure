454bd21949ba999f0fbd553a3ea214febfca2dc7
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-07-14 16:22:02 +0200

    Update styling in aboutUs Page

--- frontend/src/pages/AboutUs/index.jsx
+++ frontend/src/pages/AboutUs/index.jsx
@@ -1,21 +1,20 @@
-import ImageTrip from "../../components/ImageTrip/ImageTrip.jsx";
+import TeamCard from "../../components/TeamCard/TeamCard";
+import Trip from "../../components/Trip/Trip";
+import ContactUsCard from "../../components/ContactUsCard/ContactUsCard";
 const AboutUs = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full bg-KayakHomeHeader h-96 bg-no-repeat bg-cover">
-      <div className="flex-col text-venture-white my-6">
-        <h1>ORGANIZE YOUR PERFECT DAY-TRIP</h1>
-        <h3>BY USERS, FOR USERS</h3>
+    <div className="h-[87.5vh] flex flex-col gap-20">
+      <div className="flex flex-col items-center justify-center w-full bg-BgAboutUs h-96 bg-no-repeat bg-cover">
+        <div className="flex-col text-venture-white my-6">
+          <h1>ORGANIZE YOUR PERFECT DAY-TRIP</h1>
+          <h3>BY USERS, FOR USERS</h3>
+        </div>
       </div>
-      <div className="flex flex-row w-full justify-center p-8">
-        <div className="flex flex-col w-1/3 items-center">
-          <ImageTrip className="text-4xl my-3" />
-        </div>
-        <div className="flex flex-col w-1/3 items-center">
-          <ImageTrip className="text-4xl my-3" />
-        </div>
-        <div className="flex flex-col w-1/3 items-center">
-          <ImageTrip className="text-4xl my-3" />
-        </div>
+
+      <div className=" flex flex-row w-full justify-evenly p-8">
+        <TeamCard />
+        <Trip />
+        <ContactUsCard />
       </div>
     </div>
   );
