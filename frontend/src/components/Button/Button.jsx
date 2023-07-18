93209ffc89b3447a7e50b2e3c7d7359fe06a9571
Aleksandar Ilic <aleks.i6@hotmail.com>
2023-07-18 10:32:34 +0200

    issues solved with Button component

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,11 +1,11 @@
-const Button = ({ type, onClick, children }) => {
+const Button = ({ type, onClickFunction, children }) => {
   return (
     <button
       className={
         "bg-venture-green rounded-full px-5 py-2 whitespace-nowrap text-venture-white hover:bg-venture-green-hovered"
       }
       type={type}
-      onClick={onClick}
+      onClick={onClickFunction}
     >
       {children}
     </button>
--- frontend/src/pages/NewTrip/index.jsx
+++ frontend/src/pages/NewTrip/index.jsx
@@ -1,43 +1,37 @@
-import React from 'react';
+//import React from 'react';
 import InputField from "../../components/InputField/InputField";
 import Button from "../../components/Button/Button";
 import {MdOutlineKeyboardArrowDown} from "react-icons/md";
 import {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {add_trip} from "../../store/slices/newTrip";
+import {Link} from "react-router-dom";
 //import TripHeader from "../../components/TripHeader/TripHeader";
 //import GoogleMapReact from "google-map-react";
 //import { Autocomplete } from "@react-google-maps/api";
 
 const NewTrip = () => {
-    const [newTrip, setNewTrip] = useState('')
-    const [newTripTest, setNewTripTest] = useState('')
+    const [tripName, setTripName] = useState('')
+    const [startTime, setStartTime] = useState('')
+    //const [description, setDescription] = useState('')
+    //const [categories, setCategories] = useState([])
+    //const [dayOfTrip, set dayOfTrip] = useState('')
     const dispatch = useDispatch()
     const selectedItems = useSelector((state) => console.log(state.newTrip, "USESELECT"))
     //const coordinates = { lat: 46.807405, lng: 8.223595 };
 
     const handleAddTrip = (e) => {
         e.preventDefault()
-        if (newTrip.trim()) {
+        if (tripName.trim()) {
             dispatch(
                 add_trip({
-                    tripName: newTrip,
-                    startTime: newTripTest,
+                    tripName: tripName,
+                    startTime: startTime,
                 })
             );
         }
     }
 
-    const handleInput = (e) => {
-        // console.log(e)
-        setNewTrip(e)
-
-    }
-    const handleInputTest = (e) => {
-        // console.log(e)
-
-        setNewTripTest(e)
-    }
 
     return (
         <>
@@ -71,20 +65,22 @@ const NewTrip = () => {
                         <div className="flex flex-row justify-center gap-5 ">
                             <InputField
                                 type={"text"}
-                                value={newTrip}
-                                onChange={handleInput}
+                                value={tripName}
+                                onChange={(e) => setTripName(e)}
                                 id={"tripName"}
                                 className="flex flex-row w-full "
                                 placeholder={"Write the name of your trip"}
                             />
                             <InputField
                                 type={"text"}
-                                value={newTripTest}
-                                onChange={handleInputTest}
+                                value={startTime}
+                                onChange={(e) => setStartTime(e)}
                                 className="flex flex-row w-full "
                                 placeholder={"START TIME"}
                             />
-                            <Button type={"submit"}>CONFIRM</Button>
+                            <Link to="/" type={"submit"} className="flex gap-4">
+                                <Button >CONFIRM</Button>
+                            </Link>
                         </div>
                     </form>
                 </div>
