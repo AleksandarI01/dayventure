63defa45b2339d1ce7a20c975afbe388496ef498
Aleksandar Ilic <aleks.i6@hotmail.com>
2023-07-18 16:45:58 +0200

    ProcessBar created, error messages throughout Signup Process working, css of Label component working on different conditions.

--- frontend/src/components/SignUp/SignUpFirstStep.jsx
+++ frontend/src/components/SignUp/SignUpFirstStep.jsx
@@ -1,16 +1,15 @@
-import { useState } from "react";
+import {useState} from "react";
 import Button from "../Button/Button";
 import InputField from '../InputField/InputField';
-import AnimatedSignUpImage from "../AnimatedSignUpImage/AnimatedSignUpImage";
-import { MdEmail } from 'react-icons/md';
+import {MdEmail} from 'react-icons/md';
 import {axiosDayVenture} from "../../axios/index.js";
 
-const SignUpFirstStep = ({ setEmail, moveToNextStep }) => {
-  const [email, setEmailInput] = useState("");
-  const [errorMessage, setErrorMessage] = useState();
+const SignUpFirstStep = ({setEmail, moveToNextStep}) => {
+    const [email, setEmailInput] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
-  const handleOnClick = (e) => {
-    e.preventDefault();
+    const handleOnClick = (e) => {
+        e.preventDefault();
         axiosDayVenture
             .post("/registration/", {
                 email: email,
@@ -23,41 +22,43 @@ const SignUpFirstStep = ({ setEmail, moveToNextStep }) => {
                 }
             })
             .catch((err) => {
-                 if (err.request.response === '{"email":["This field may not be blank."]}') {
+                console.log(err.request.response);
+                if (err.request.response === '{"email":["This field may not be blank."],"username":["This field may not be blank."]}') {
                     setErrorMessage("The email field can not be empty!");
-                } else if (err.request.response === '{"email":["user with this email already exists."],"username":["A user with that username already exists."]}') {
+                } else if (err.request.response === '{"email":["user with this email already exists."]}') {
                     setErrorMessage("This email is already registered!");
+                } else if (err.request.response === '{"email":["Enter a valid email address."]}') {
+                    setErrorMessage("Enter a valid email address!");
                 }
-                 console.log(err);
-                 console.log(err.request.response)
+                console.log(err);
+                console.log(err.request.response)
             });
-  };
-  return (
-    <>
-      {/*<div className=" flex justify-items-end items-center gap-5 p-5 ">
+    };
+    return (
+        <>
+            {/*<div className=" flex justify-items-end items-center gap-5 p-5 ">
           <p>Are you already a dayventurer?</p>
           <Button>LOGIN</Button>
         </div>*/}
-      <div className="flex-grow flex flex-col justify-center items-center gap-20">
-        <h1>Sign Up</h1>
-        <InputField
-          type="email"
-          placeholder="Email"
-          icon={MdEmail}
-          color={errorMessage ? "venture-red": "venture-green"}
-          value={email}
-          onChange={setEmailInput}
-        />
-        {errorMessage && (
-            <div className={"w-full h-1"}>
-              <p className="signup-erro-message text-venture-red">{errorMessage}</p>
+            <div className="flex-grow flex flex-col justify-center items-center gap-20">
+                <h1>Sign Up</h1>
+                <InputField
+                    type="email"
+                    placeholder="Email"
+                    icon={MdEmail}
+                    color={errorMessage ? "venture-red" : "venture-green"}
+                    value={email}
+                    onChange={setEmailInput}
+                />
+                <div className={"w-full h-1"}>
+                    {errorMessage && (
+                        <p className="signup-erro-message text-venture-red">{errorMessage}</p>
+                    )}
+                </div>
+                <Button onClickFunction={handleOnClick}>Sign Up</Button>
             </div>
-        )}
-        <AnimatedSignUpImage />
-        <Button onClickFunction={handleOnClick}>Sign Up</Button>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default SignUpFirstStep;
