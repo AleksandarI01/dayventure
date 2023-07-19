1a39c3bc9440a71e2a8d8dab8678fd08e15ed3d2
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-07-19 21:56:16 +0200

    Update notification modal styling

--- frontend/src/components/UserNotifications/UserNotifications.jsx
+++ frontend/src/components/UserNotifications/UserNotifications.jsx
@@ -1,15 +1,40 @@
+import React from "react";
 import FriendRequest from "../FriendRequest/FriendRequest";
 import Notification from "../Notification/Notification.jsx";
-const UserNotifications = ({notifications, friendRequests, setLoadNotifications}) => {
+
+
+const getBorderStyle = (index, arrayLength) =>
+  index < arrayLength - 1 ? "border-b border-gray-200 pb-2" : "";
+
+const UserNotifications = ({
+  notifications,
+  friendRequests,
+  setLoadNotifications,
+}) => {
   return (
     <>
-      <div className="fixed top-20 right-2 w-2/12 border-2 bg-venture-white pr-1 pl-1 pb-2 pt-2">
-        <div className="flex flex-row justify-center ">
+      <div className=" absolute top-20 right-2 w-11/12 sm:right-4 sm:w-9/12 md:right-6 md:w-1/2 lg:right-10 lg:w-2/5 xl:right-13 xl:max-w-xl border-2 rounded bg-venture-white pr-1 pl-1 pb-1 pt-2 mt-2"
+      >
+        <div className="flex justify-around bg-venture-green text-venture-white rounded p-1">
+          <h4>Notifications ({notifications.length + friendRequests.length})</h4>
+          <button>Mark all as Read</button>
+        </div>
+        <div className="flex flex-row justify-center py-2 ">
           {friendRequests.length > 0 && <h5>FRIEND REQUESTS</h5>}
         </div>
-        <div className="flex flex-row justify-center pb-5">
-          <div className="flex flex-col w-full">
-            {friendRequests.map((friendRequest) => <FriendRequest key={friendRequest.id} friendRequest={friendRequest} setLoadNotifications={setLoadNotifications}/>)}
+        <div className="flex flex-row justify-center pb-5 ">
+          <div className="flex flex-col w-[100%]">
+            {friendRequests.map((friendRequest, index) => (
+              <div
+                key={friendRequest.id}
+                className={getBorderStyle(index, friendRequests.length)}
+              >
+                <FriendRequest
+                  friendRequest={friendRequest}
+                  setLoadNotifications={setLoadNotifications}
+                />
+              </div>
+            ))}
           </div>
         </div>
         <div className="flex flex-row justify-center">
@@ -19,8 +44,21 @@ const UserNotifications = ({notifications, friendRequests, setLoadNotifications}
         </div>
         <div className="flex flex-row justify-center pb-5">
           <div className="flex flex-col w-full">
-            {notifications.map((notification) => <Notification key={notification.id} notification={notification} setLoadNotifications={setLoadNotifications}/>)}
+            {notifications.map((notification, index) => (
+              <div
+                key={notification.id}
+                className={getBorderStyle(index, notifications.length)}
+              >
+                <Notification
+                  notification={notification}
+                  setLoadNotifications={setLoadNotifications}
+                />
+              </div>
+            ))}
           </div>
+        </div>
+        <div className="bg-venture-green text-venture-white rounded p-1 mb-1">
+          <button>View All</button>
         </div>
       </div>
     </>
