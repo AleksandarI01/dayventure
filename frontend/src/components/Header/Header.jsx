789abb8e4e6fb41a2e6ff04ae5143753136d1e1f
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-07-19 21:55:40 +0200

    Update Notification Bell

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,10 +2,12 @@ import { NavLink, useNavigate, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../store/slices/user.js";
 import { useEffect, useState } from "react";
-import { AiFillBell } from "react-icons/ai";
+import { MdNotifications } from "react-icons/md";
 import UserNotifications from "../UserNotifications/UserNotifications.jsx";
 import Button from "../Button/Button.jsx";
-import {axiosDayVenture} from "../../axios/index.js";
+import { axiosDayVenture } from "../../axios/index.js";
+import Arrow from '../Arrow/Arrow.jsx';
+
 
 const Header = () => {
   const navigate = useNavigate();
@@ -16,7 +18,7 @@ const Header = () => {
   const [notifications, setNotifications] = useState([]);
   const [friendRequests, setFriendRequests] = useState([]);
   const [notificationNumber, setNotificationNumber] = useState(0);
-  const [loadNotifications, setLoadNotifications] = useState(0)
+  const [loadNotifications, setLoadNotifications] = useState(0);
 
   useEffect(() => {
     SetMenuItems([
@@ -29,46 +31,50 @@ const Header = () => {
 
   useEffect(() => {
     if (accessToken) {
-      setNotificationNumber(0)
-        const config = {headers: {Authorization: `Bearer ${accessToken}`}};
+      setNotificationNumber(0);
+      const config = { headers: { Authorization: `Bearer ${accessToken}` } };
       axiosDayVenture
-          .get("/friends/requests/", config)
-          .then((res) => {
-            setFriendRequests(res.data);
-            setNotificationNumber(prevState => prevState + res.data.length);
-            console.log(res.data)
-          })
-          .catch((error) => {
-            console.log(error);
-          })
+        .get("/friends/requests/", config)
+        .then((res) => {
+          setFriendRequests(res.data);
+          setNotificationNumber((prevState) => prevState + res.data.length);
+          console.log(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
       axiosDayVenture
-          .get("/notifications/", config)
-          .then((res) => {
-            setNotifications(res.data);
-            setNotificationNumber(prevState => prevState + res.data.length)
-          })
-          .catch((error) => {
-            console.log(error);
-          })
-
+        .get("/notifications/", config)
+        .then((res) => {
+          setNotifications(res.data);
+          setNotificationNumber((prevState) => prevState + res.data.length);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-  }, [accessToken, loadNotifications])
+  }, [accessToken, loadNotifications]);
 
   const handleLogout = (e) => {
     e.preventDefault();
     dispatch(logout());
     localStorage.removeItem("refreshToken");
-    setShowNotifications(false)
+    setShowNotifications(false);
     navigate("/");
   };
 
-  const handleViewNotifications = (event) => {
-    event.preventDefault();
+  const handleViewNotifications = (e) => {
+    e.preventDefault();
     setShowNotifications(!showNotifications);
   };
 
+  const processNotificationNumber = (num) => {
+    return num > 9 ? '9+' : num;
+  };
+
   return (
-    <header className="flex justify-between border-b-venture-black relative p-5 border-b border-solid">
+    <>
+    <header className="flex justify-between border-b-venture-black p-5 border-b border-solid z-10">
       <Link to="/" className="flex gap-4">
         <h4>dayventure</h4>
       </Link>
@@ -76,23 +82,52 @@ const Header = () => {
         <ul className="flex gap-[50px] items-center">
           {MenuItems.map((item, idx) => (
             <li key={idx}>
-              <NavLink to={item.link} className={"hover:text-venture-green-hovered"}>{item.name}</NavLink>
+              <NavLink
+                to={item.link}
+                className={"hover:text-venture-green-hovered"}
+              >
+                {item.name}
+              </NavLink>
             </li>
           ))}
         </ul>
         {accessToken ? (
           <div className="flex items-center gap-10">
-            <AiFillBell
-              onClick={handleViewNotifications}
-              className="text-2xl text-venture-black cursor-pointer"
-            />
-              {notificationNumber > 0 ? <p>{notificationNumber}</p>: null}  {/*todo: show this in red circle over bell icon*/}
+            <div style={{ position: 'relative' }}>
+                <div style={{ position: 'relative', display: 'inline-block' }}>
+                  <MdNotifications
+                    onClick={handleViewNotifications}
+                    className="text-3xl text-venture-black cursor-pointer"
+
+                  />
+                  {notificationNumber > 0 && (
+                    <div
+                      style={{
+                        position: 'absolute',
+                        top: '-6px',
+                        right: '-6px',
+                        background: 'red',
+                        borderRadius: '50%',
+                        width: '19px',
+                        height: '19px',
+                        color: 'white',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <p>{processNotificationNumber(notificationNumber)}</p>
+                    </div>
+                  )}
+                  {showNotifications && <Arrow />}
+                </div>
+              </div>
             <Button
-            className="text-base text-venture-white uppercase bg-venture-green cursor-pointer px-5 py-2 border-[none]"
-            onClickFunction={handleLogout}
-          >
-            Logout
-          </Button>
+              className="text-base text-venture-white uppercase bg-venture-green cursor-pointer px-5 py-2 border-[none]"
+              onClickFunction={handleLogout}
+            >
+              Logout
+            </Button>
           </div>
         ) : (
           <div className="flex gap-0.5">
@@ -115,8 +150,17 @@ const Header = () => {
           </div>
         )}
       </nav>
-      {showNotifications ? <UserNotifications notifications={notifications} friendRequests={friendRequests} setLoadNotifications={setLoadNotifications}/> : ""}
+      
+      
     </header>
+    {showNotifications && (
+        <UserNotifications
+          notifications={notifications}
+          friendRequests={friendRequests}
+          setLoadNotifications={setLoadNotifications}
+        />
+      )}
+    </>
   );
 };
 
